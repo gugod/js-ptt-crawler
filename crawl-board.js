03f366da0f06afc50584d6b426fe42f9dc3115c5
Kang-min Liu <gugod@gugod.org>
2015-10-15 22:09:09 +0200

    init

---
+++ crawl-board.js
@@ -0,0 +1,53 @@
+var https = require('https'),
+    cheerio = require('cheerio');
+
+var ptt_url = "https://www.ptt.cc";
+
+function ptt_get(url, cb) {
+    var req = https.get(url, function(res) {
+        var out = "";
+        res.on('data', function(chunk) {
+            out = out + chunk;
+        });
+        res.on('end', function() {
+            cb(out);
+        })
+    });
+    req.end();
+}
+
+function harvest_board_indices(board_url, board_name, cb) {
+    ptt_get(
+        board_url,
+        function(body) {
+            var $ = cheerio.load(body);
+            var ret = [];
+            $("a[href^='/bbs/" + board_name + "/index']").each(function(i, el) {
+                var href = $(el).attr("href");
+                var m;
+                if (m = href.match(/index([0-9]+)\.html/)) {
+                    ret.push({
+                        page_number: m[1],
+                        url: ptt_url + href
+                    });
+                }
+            });
+            cb(ret);
+        }
+    )
+}
+
+if ( process.argv.length != 4) {
+    process.abort();
+}
+
+var board_name = process.argv[2];
+var output_dir = process.argv[3];
+
+harvest_board_indices(
+    ptt_url + "/bbs/" + board_name + "/index.html",
+    board_name,
+    function(board_indices) {
+        console.log(board_indices);
+    }
+);
